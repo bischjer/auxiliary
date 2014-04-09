f7b84b313a36bc8d3b7eb30322db7d543803a3a1
bischjer <bischjer@protojour.com>
2014-04-09 14:33:42 +0200

    moved 'unit' tests to integration

--- aux/protocols/http/http.py
+++ aux/protocols/http/http.py
@@ -1,6 +1,44 @@
 from aux.protocols.connection import TCPConnection
 from urlparse import urlparse, urlunparse
 
+HTTP_DEFAULT_PORT = 80
+TCP_FRAME_BUFFER_SIZE = 1500#bytes
+
+class HTTPProtocol(object):
+    __is_persistent = False
+    __transport_frame_size = TCP_FRAME_BUFFER_SIZE
+
+    def __init__(self, raw_url):
+        self.url = self.set_url_from_string(raw_url)
+        self.__connection = TCPConnection(self.url.hostname, self.url.port) #TODO: factory with persist scheme
+        self.__connection.connect()
+
+    def is_persistent(self):
+        return self.__is_persistent
+
+    def set_url_from_string(self, raw_url):
+        url = urlparse(raw_url)
+        if not url.port:
+            l = list(url)
+            l[1] = l[1] + ":%i" % HTTP_DEFAULT_PORT
+            url = urlparse(urlunparse(l))        
+        return url
+
+    
+    def receive_response(self, response):
+        #rx--
+        pass
+    
+    
+    def send_request(self, request):
+        request.target = self.url.hostname
+        #tx--
+        self.__connection.send(str(request))
+        
+
+
+
+    
 class HTTPConnection(object):
 
     __is_persistent = False
