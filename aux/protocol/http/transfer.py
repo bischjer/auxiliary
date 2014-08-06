ad341f942a0b6bc19de4440e53ef6a8fb0388eda
bischjer <bischjer@protojour.com>
2014-08-06 16:11:26 +0200

    http receive tests

--- aux/protocol/http/transfer.py
+++ aux/protocol/http/transfer.py
@@ -3,69 +3,53 @@ import re
 
 class DefaultController(object):
 
-    def __init__(self, transport, msg):
+    def __init__(self, headers, transport, msg):
+        self.headers = headers
         self.transport = transport
         self.msg = msg
 
     def read(self):
-        raw_response = "\n".join(self.msg)
-        in_buf = ""
-        while 1:
-            print "default controller"
-            try:
-                in_buf = self.transport.recv()
-            except Exception, e:
-                print e.message
-            if len(in_buf) < 1:
-                break
-            raw_response = raw_response + in_buf
-        return raw_response
-        
+        content_length = int(self.headers.get('Content-Length', 0))
+        return self.msg[0: content_length]
+
+class NoContentController(object):
+    def __init__(self, headers, transport, msg):
+        self.headers = headers
+        self.transport = transport
+        self.msg = msg
+
+    def read(self):
+        return self.msg
+    
 class ChunkedController(object):
 
-    def __init__(self, transport, msg):
+    def __init__(self, headers, transport, msg):
+        self.headers = headers
         self.transport = transport
         self.msg = msg
-
-    def chunked_parser(self, raw_response):
-        re_chunk = re.compile(r'^([a-f|\d]{1,4})\r')
-        #TODO: fix this horrible impl.
-        print raw_response
-        response = ""
-        data = raw_response.split('\n')
-        curr_line = ""
-        # print "chunked_parser_entry"
-        for next_line in data:
-            is_next_a_chunk = re_chunk.findall(next_line)
-            if len(is_next_a_chunk) > 0:
-                next_chunk = int(is_next_a_chunk[0], 16)
-                curr_line.rstrip()
-            else:
-                curr_line = next_line                
-                response += curr_line
-        return response
         
     def read(self):
-        re_chunk = re.compile(r'^([a-f|\d]+){1,4}\r\n')
-        raw_response = ""
-        in_buf = "\n".join(self.msg)
-
-        raw_response = raw_response + in_buf
-
+        re_chunk = re.compile(r'^([a-f|\d]{1,4})\r\n')
+        re_end_chunk = re.compile(r'^0\r\n\r\n0')
+        #TODO: fix this horrible impl.
+        raw_response = self.msg
+        response = ""
         while 1:
-            try:
-                in_buf = self.transport.recv(TCP_DEFAULT_FRAME_SIZE)
-            except Exception, e:
-                print e.message
-            fa = re_chunk.findall(in_buf)
-            raw_response += in_buf                
-            if len(fa) > 0:
-                if fa[0] == '0':
-                    raw_response += in_buf                
-                    break;
-        return self.chunked_parser(raw_response)
+            next_chunk = re_chunk.findall(raw_response[0:8])
+            end_chunk = re_end_chunk.findall(raw_response[0:8])
+            if len(next_chunk) > 0:
+                if int(next_chunk[0], 16) == 0 or len(end_chunk) > 0:
+                    break
+                raw_response = raw_response[len(next_chunk[0])+2:]
+                response += raw_response[:int(next_chunk[0], 16)]
+                raw_response = raw_response[int(next_chunk[0], 16)+2:]
+        return response
 
 def transferFactory(headers):
+    content_length = headers.get('Content-Length', None)
+    if content_length != None:
+        if int(content_length) < 1:
+            return NoContentController
     content_type = headers.get('Transfer-Encoding', None)
     if content_type != None:
         if 'chunked' in content_type.lower():
---
+++ test/unit/test_http_receive.py
@@ -0,0 +1,77 @@
+from unittest2 import TestCase
+from aux.protocol.http.http import HTTP
+import struct
+import os
+
+class FakeTransport(object):
+
+    def __init__(self, message):
+        self.fake_message = message
+        self.bytes_read = 0
+        
+    def recv(self, nofchar=1200):
+        buffer = ""
+        for n in xrange(0, nofchar):
+            if self.bytes_read >= len(self.fake_message):
+                break
+            else:
+                buffer += self.fake_message[self.bytes_read]
+            self.bytes_read += 1
+        return buffer
+    
+    def close(self):
+        pass
+
+
+class HTTP_RECEIVE_TEST(TestCase):
+
+    def test_receive_200_startline_only(self):
+        message = "HTTP/1.1 200 OK\r\n"
+        http = HTTP()
+        response = http.receive(FakeTransport(message))
+        self.assertEquals(response.status,
+                          200)
+
+    def test_receive_200_only_headers(self):
+        message = """HTTP/1.1 200 OK\r\nServer: nginx/1.5.13\r\nDate: Sat, 02 Aug 2014 19:40:38 GMT\r\nContent-Type: text/html\r\nContent-Length: 0\r\nLast-Modified: Mon, 14 Apr 2014 08:38:26 GMT\r\nConnection: keep-alive\r\nExpires: Sat, 02 Aug 2014 20:40:38 GMT\r\nCache-Control: max-age=3600\r\nAccept-Ranges: bytes\r\n\r\n"""
+        http = HTTP()
+        response = http.receive(FakeTransport(message))
+        self.assertEquals(len(response.body), 0)
+        self.assertEquals(len(response.headers), 9)
+                          
+    def test_receive_200_with_json_body(self):
+        message = """HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: 15\r\n\r\n{{Hello:world}}"""
+        http = HTTP()
+        response = http.receive(FakeTransport(message))
+        self.assertEqual(len(response.body), 15)
+
+    def test_receive_200_with_chunked_no_body(self):
+        message = """HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nTransfer-Encoding : chunked\r\n\r\n0\r\n\r\n0"""
+        http = HTTP()
+        response = http.receive(FakeTransport(message))
+        self.assertEqual(len(response.body), 0)
+
+        
+    def test_receive_200_with_chunked_body(self):
+        message = """HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nTransfer-Encoding : chunked\r\n\r\n1a\r\nABCDEFGHIJKLMNOPQRSTUVWXYZ\r\n0\r\n\r\n0"""
+        http = HTTP()
+        response = http.receive(FakeTransport(message))
+        self.assertEqual(len(response.body), 26)
+
+    def test_receive_200_with_chunked_multi_body(self):
+        message = """HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nTransfer-Encoding : chunked\r\n\r\n1a\r\nABCDEFGHIJKLMNOPQRSTUVWXYZ\r\n34\r\nABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ\r\n34\r\nABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ\r\n34\r\nABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ\r\n34\r\nABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ\r\n0\r\n\r\n0"""
+        http = HTTP()
+        response = http.receive(FakeTransport(message))
+        self.assertEqual(len(response.body), 234)
+
+    def test_receive_200_with_chunked_binary_body(self):
+        byte_range = 256
+        data = "".join([struct.pack('B', i) for i in xrange(0,byte_range)])
+        message = """HTTP/1.1 200 OK\r\nContent-Type: application/zip;charset=UTF-8\r\nTransfer-Encoding : chunked\r\nContent-Disposition : attachment; filename="test_chunkbin.zip"\r\n\r\n100\r\n%s\r\n0\r\n\r\n0""" % data
+        http = HTTP()
+        response = http.receive(FakeTransport(message))
+        self.assertEqual(response.body, "/tmp/aux/test_chunkbin.zip")
+        self.assertTrue(os.path.exists(response.body))
+        self.assertEqual(os.path.getsize(response.body), byte_range)
+        
+        
