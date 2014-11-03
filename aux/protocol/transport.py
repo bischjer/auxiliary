4936412394f61959597c9ed41eadfbfe78c0c2dc
bischjer <bischjer@protojour.com>
2014-11-03 15:13:02 +0100

    need to find bug in chunked with body and only 0 length terminator

--- aux/protocol/transport.py
+++ aux/protocol/transport.py
@@ -1,7 +1,8 @@
 from socket import ( socket, AF_INET, SOCK_DGRAM,
                      IPPROTO_TCP, SOCK_STREAM,
-                     SOL_SOCKET, SO_REUSEADDR)
-from ssl import wrap_socket, CERT_NONE, SSLError
+                     SOL_SOCKET, SO_REUSEADDR )
+import ssl
+# from ssl import wrap_socket, CERT_NONE, SSLError, PROTOCOL_SSLv23
 
 TCP_DEFAULT_FRAME_SIZE = 1200
 
@@ -57,32 +58,43 @@ class CertificationHostnameMismatch(Exception):pass
 
 class TLS_TCPTransport(TCPTransport):
     #TODO: This should just be a wrapper 
-    def __init__(self, hostname, port, timeout=1):
+    def __init__(self, hostname, port, timeout=0):
         super(TLS_TCPTransport, self).__init__(hostname, port)
 
         self.disable_ssl_certificate_validation = True
+        self.timeout = timeout
         #TODO: Should do a better build up of ssl_socket
-        self.__connection = wrap_socket(socket(AF_INET, SOCK_STREAM),
-                                        cert_reqs=CERT_NONE,
-                                        do_handshake_on_connect=False)
+        self.__connection = ssl.wrap_socket(socket(AF_INET, SOCK_STREAM),
+                                            keyfile=None,
+                                            certfile=None,
+                                            server_side=False,
+                                            cert_reqs=ssl.CERT_NONE,
+                                            ssl_version=ssl.PROTOCOL_SSLv23,
+                                            ca_certs=None,
+                                            do_handshake_on_connect=True,
+                                            suppress_ragged_eofs=True,
+                                            ciphers=None)
         self.__connection.setsockopt(SOL_SOCKET,
                                      SO_REUSEADDR,
                                      1)
-        self.__connection.settimeout(timeout)
+        self.__connection.settimeout(self.timeout)
         
     def connect(self):
         try:
             self.__connection.connect(self.addr)
-
             if not self.disable_ssl_certificate_validation:
                 cert = self.__connection.getpeercert()
+                print cert
                 hostname = "hello"
                 if not self._validateCertificateHostname(cert, hostname):
                     raise CertificationHostnameMismatch(
                         'Server presented certificate that does not match ')
             
-        except SSLError, e:
+        except Exception, e:
             print e
+            if self.__connection:
+                self.__connection.close()
+        
         
     def send(self, message):
         self.__connection.write(message)
