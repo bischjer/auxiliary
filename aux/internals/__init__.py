6f31fb332bea2ebf8dca0eaf8dad63d33f845a10
Bjorn Schjerve <bischjer@gmail.com>
2015-02-15 00:55:51 +0100

    create plugin service

--- aux/internals/__init__.py
+++ aux/internals/__init__.py
@@ -1,4 +1,6 @@
-
+from aux import base_dir
+import os
+import sys
 
 def plugin_creator_routine(plugincreator, arguments):
     # print plugincreator, arguments    
@@ -6,17 +8,17 @@ def plugin_creator_routine(plugincreator, arguments):
         if len(arguments) > 0:
             packagename = "_".join(['aux', 'service', arguments[0]])
             print packagename
-        os.system('paster create -t basic_package -o %s' % base_dir()+"/..")
+        os.system('paster create -t basic_package -o %s --no-interactive %s' % (base_dir()+"/..", packagename) )
         sys.exit(0)
     elif 'device' in plugincreator:
         if len(arguments) > 0:
             packagename = "_".join(['aux', 'device', arguments[0]])
             print packagename
-        os.system('paster create -t basic_package -o %s' % base_dir()+"/..")
+        os.system('paster create -t basic_package -o %s --no-interactive %s' % (base_dir()+"/..", packagename))
         sys.exit(0)
     elif 'protocol' in plugincreator:
         if len(arguments) > 0:
             packagename = "_".join(['aux', 'protocol', arguments[0]])
             print packagename
-        os.system('paster create -t basic_package -o %s' % base_dir()+"/..")
+        os.system('paster create -t basic_package -o %s --no-interactive %s' % (base_dir()+"/..", packagename))
         sys.exit(0)
