8460ec6a1025ea86f9e55189460122cf1934c899
bischjer <bischjer@protojour.com>
2015-03-05 15:11:11 +0100

    started looking into get_system plugin probe

--- aux/system/__init__.py
+++ aux/system/__init__.py
@@ -1,10 +1,36 @@
+# from aux.internals.pluginhook import PluginImporter
+import sys
+import os
+import imp
+import device
+import service
+
 #these imports should be kept in a system pool and only be instantiated once.
 
-def get_system(systemjson):
+def get_system(systemjson=None, systemtype=None):
+    print systemjson
+    print systemtype
 
-    systemtype = systemjson.get('systemtype', False)
-    if systemtype:
-        if systemtype == 'TODO: do system lookup from this point, must include external modules, figure it out':
-            print "do it"
+    ## NEED TO LOOKUP IN system.device and system.service and aux_device and aux_service
+    devicemodule = os.path.dirname(device.__file__)
+    servicemodule = os.path.dirname(service.__file__)
+    
+    r_filters = ['ext', '__init__.py', '__init__.pyc']
+    print [r for r in os.listdir(devicemodule) if r not in r_filters]
+    print [r for r in os.listdir(servicemodule) if r not in r_filters]
+    
+    print [p for p in sys.path if 'aux_device_' in p]
+    print [p for p in sys.path if 'aux_service_' in p]
+    
+    
+    # print sys.path
+    # pir = PluginImporter(['aux_service_'],__name__)
+    # print pir.find_module('kezzler')
+    # print [a for a in sys.path if 'aux_service_' in a]
+    
+    # systemtype = systemjson.get('systemtype', False)
+    # if systemtype:
+    #     if systemtype == 'TODO: do system lookup from this point, must include external modules, figure it out':
+    #         print "do it"
     
     return None
