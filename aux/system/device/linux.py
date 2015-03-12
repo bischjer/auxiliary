7668aa2908cf9c2033b52f9d9d75d38b9d163fd1
bischjer <bischjer@protojour.com>
2015-03-12 13:19:07 +0100

    linuxdevice

--- aux/system/device/linux.py
+++ aux/system/device/linux.py
@@ -3,9 +3,9 @@ from aux.authentication import BaseCredentials
 from aux.api import ssh
 
 class LinuxDevice(BaseSystem):
-    def __init__(self, identifier):
-        super(LinuxDevice, self).__init__(identifier)
-
+    def __init__(self, hostname):
+        super(LinuxDevice, self).__init__(hostname)
+        self.hostname = self.identifier
         self.ssh = ssh
         # self.ssh.set_hostname(self.identifier)
 
